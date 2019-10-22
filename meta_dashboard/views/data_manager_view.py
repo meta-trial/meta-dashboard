eae9cca15154c392e02d5edab23e498de19c8d45
erikvw <ew2789@gmail.com>
2019-10-22 01:49:50 -0500

    remove unused files, update navbar and urls

--- meta_dashboard/views/data_manager_view.py
+++
@@ -1,6 +0,0 @@
-from edc_data_manager.views import HomeView as Base
-
-
-class HomeView(Base):
-
-    navbar_selected_item = "data_manager_home"
--- meta_dashboard/views/subject_review_listboard.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from edc_review_dashboard.views import SubjectReviewListboardView as Base
-from edc_subject_model_wrappers import SubjectVisitModelWrapper
-
-
-class SubjectReviewListboardView(Base):
-
-    listboard_model = settings.SUBJECT_VISIT_MODEL
-    model_wrapper_cls = SubjectVisitModelWrapper
